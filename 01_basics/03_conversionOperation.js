1190896ee50b0c7ff4617eeafd499e01c51a8ae3
Ahmed Raza <80670768+razakhan2526@users.noreply.github.com>
2024-07-18 15:04:51 +0000

    Conversion Operators & Postfix, Prefix Operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -5,7 +5,7 @@ let score =  33
 // let score = true;
 // let score = "Ahmed Raza";
 
-console.log(typeof score);
+// console.log(typeof score);
 // console.log(typeof score);
 // console.log(typeof(score)); // optional
 
@@ -38,5 +38,58 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 33;
 
 let StringNumber = String(someNumber);
-console.log(typeof StringNumber);
-console.log(StringNumber);
+// console.log(typeof StringNumber);
+// console.log(StringNumber);
+
+// ------------------------------------- Operations ------------------------------- //
+
+let value = 3
+let negvalue = -value;
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Ahmed";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+"");
+
+
+// Prefix & Postfix OPerators
+
+/** 
+ * Postfix 
+ * Increment Value
+**/
+
+let x = 3;
+let y = x++;
+console.log(`x:${x}, y:${x++}`);
+
+/** 
+ * Prefix
+ * Increment Value
+**/
+
+let x2 = 3;
+let y2 = ++x2;
+console.log(`x2:${x2}, y2:${++x2}`);
+
+
+
+
+
+
