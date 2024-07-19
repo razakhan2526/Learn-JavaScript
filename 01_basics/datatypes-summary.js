bb2c3b6dcb79f7fb27bdc267bae4bc5e99c43bbe
Ahmed Raza <80670768+razakhan2526@users.noreply.github.com>
2024-07-19 16:11:14 +0000

    change location

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,34 @@
+// Primitive Data Types
+
+// *String, Number, Bigint, Boolean, Undefined, Null, Symbol*
+
+const score = 100;
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const BigNumber = 1234567890n;
+
+// Non Primitive Data Types
+
+// *Object, Array, Functions*
+
+const heros = ['osamn', 'artugrul', 'turgut'];
+
+let myobj = {
+    name: "ahmed",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof BigNumber);
